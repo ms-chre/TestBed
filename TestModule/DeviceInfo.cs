d624a6f02891926d8c3dbe36049d68e163c99077
Chethan Reddy <chre@microsoft.com>
2021-01-07 11:24:10 +0530

    Add project files.

---
+++ TestModule/DeviceInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestModule
+{
+    public class DeviceInfo
+    {
+        public string name { get; set; }
+    }
+}
